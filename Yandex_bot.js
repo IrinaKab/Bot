1cdd7b6f11f974eb0f9ba341ee29fc2ade43ebf6
IrinaKab <83393107+IrinaKab@users.noreply.github.com>
2021-05-23 18:56:12 +0300

    Обновление функционала бота
    1. Переходит на целевой сайт и ходит по ссылкам, с вероятностью 30% переходит обратно в Яндекс
    2. Вбивает фразу по буквам
    3. Имеет ограничение на глубину поиска не далее 5 страницы

--- Yandex_bot.js
+++ Yandex_bot.js
@@ -1,82 +1,78 @@
-
-Tampermonkey® by Jan Biniok
-v4.12
-	
-Bot for Yandex
-by You
-1
 // ==UserScript==
-2
-// @name         Bot for Yandex
-3
+// @name         Bot for Yandex за 2021-04-27
 // @namespace    http://tampermonkey.net/
-4
 // @version      0.1
-5
 // @description  try to take over the world!
-6
 // @author       You
-7
 // @match        https://yandex.ru/*
-8
-// @icon         
-9
+// @match		 https://xn----7sbab5aqcbiddtdj1e1g.xn--p1ai/*
+// @icon
 // @grant        none
-10
 // ==/UserScript==
-11
-​
-12
-let keywords=["гобой", "фагот", "как звучит флейта"];
-13
+
+let keywords=["гобой", "флюгельгорн", "как звучит флейта"];
+
 let btn=document.getElementsByClassName('button mini-suggest__button')[0];
-14
 let links=document.links;
-15
 let keyword=keywords[getRandom(0,keywords.length)];
-16
-​
-17
-​
-18
-​
-19
-​
-20
+let yandexInput = document.getElementsByName('text')[0];
+let i = 0;
+
+
 if(btn!==undefined){
-21
-    document.getElementsByName('text')[0].value=keyword;
-22
-    document.getElementsByClassName('button mini-suggest__button')[0].click();
-23
-    }else{
-24
-​
-25
-        for(let i=0; i<links.length; i++){
-26
-            if(links[i].href.indexOf('xn----7sbab5aqcbiddtdj1e1g.xn--p1ai')!=-1){
-27
-                let link=links[i];
-28
-                console.log("Нашёл фразу"+link);
-29
-                
-30
-                link.click();
-31
-                break;
-32
-            }
-33
-        }
-34
-    }
-35
-​
-36
-function getRandom(min,max){
-37
-    return Math.floor(Math.random()*(max-min)+min);
-38
+    let timerId = setInterval(()=> {
+		yandexInput.value += keyword[i];
+		i++;
+		if(i == keyword.length) {
+			clearInterval(timerId);
+			btn.click();
+		}
+	}, 1000);
+
+
+
+    }else if(location.hostname == "xn----7sbab5aqcbiddtdj1e1g.xn--p1ai" ) {
+	console.log("Мы на Музыкалка-онлайн");
+	setTimeout(()=>{
+		let index = getRandom(0,links.length);
+
+		if(getRandom(0,101)>=70) {
+			location.href = "https://www.yandex.ru/";
+		}
+		if(links[index].href.indexOf('xn----7sbab5aqcbiddtdj1e1g.xn--p1ai')!=-1)
+			links[index].click();
+	},getRandom(2000,3500));
+}
+else{
+	let nextYandexPage = true;
+	for(let i=0; i<links.length; i++) {
+		if(links[i].href.indexOf('xn----7sbab5aqcbiddtdj1e1g.xn--p1ai')!=-1) {
+			let link = links[i];
+			nextYandexPage = false;
+			console.log("Нашел фразу" + link);
+			setTimeout(()=>{
+                link.removeAttribute("target");
+                link.click();},getRandom(1000,4500));
+			break;
+		}
+	}
+
+
+
+
+
+	if(document.querySelector('.pager__item.pager__item_current_yes.pager__item_kind_page').innerText == "5") {
+		nextYandexPage = false;
+		location.href = "https://www.yandex.ru/";
+	}
+
+	if(document.querySelector('.pager__item.pager__item_current_yes.pager__item_kind_page').innerText !== "5") {
+        setTimeout(()=>{
+			document.querySelector('.pager__item_kind_next').click();}
+				   ,getRandom(3000,5000));
+	}
+}
+
+function getRandom(min,max) {
+	return Math.floor(Math.random()*(max-min)+min);
 }
